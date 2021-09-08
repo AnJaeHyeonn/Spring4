bc771fef353014e3b84848e7356e5c9bf92acafe
AnJaeHyeonn <ahn9801@naver.com>
2021-09-08 15:45:16 +0900

    09.08

--- src/main/webapp/resources/js/boardCheck.js
+++ src/main/webapp/resources/js/boardCheck.js
@@ -6,5 +6,16 @@
    alert("!!!!!!!!!!!!!");
 }
 
+/*let b1 = document.getElementById("b1");
+b1.addEventListener("click", fn1);*/
+
+function fn2() {
+	alert(document.getElementById("title").value);
+	alert(document.getElementById("contents").value);
+}
+
 let b1 = document.getElementById("b1");
-b1.addEventListener("click", fn1);
+b1.addEventListener("click", fn2);
+
+let title = document.getElementById("title").value;
+let contents = document.getElementById("contents").value;
